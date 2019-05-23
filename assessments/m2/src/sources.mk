2b1f157a05ee65541e476300cb19fb1ddab30d54
yxsyx <yshao5@ncsu.edu>
2019-05-22 21:07:28 -0600

    - Play with demos/m2/v9/Makefile, - Initial commit for assessments/m2 with issues, see git issue.

--- assessments/m2/src/sources.mk
+++ assessments/m2/src/sources.mk
@@ -10,8 +10,25 @@
 #*****************************************************************************
 
 # Add your Source files to this variable
-SOURCES =
+SOURCES = main.c\
+		  memory.c\
+		  interrupts_msp432p401r_gcc.c\
+		  startup_msp432p401r_gcc.c\
+		  system_msp432p401r.c
 
 # Add your include paths to this variable
-INCLUDES = 
+COMMONADD = ../include/common/
+MSP432ADD = ../include/msp432/
+CMSISADD = ../include/CMSIS/
+INCLUDES = $(COMMONADD)memory.h\
+		   $(COMMONADD)platform.h\
+		   $(MSP432ADD)msp432p401r.h\
+		   $(MSP432ADD)msp_compatibility.h\
+		   $(MSP432ADD)system_msp432p401r.h\
+		   $(CMSISADD)cmsis_gcc.h\
+		   $(CMSISADD)core_cm4.h\
+		   $(CMSISADD)core_cmFunc.h\
+		   $(CMSISADD)core_cmInstr.h\
+		   $(CMSISADD)core_cmSimd.h
+
 
